df249cb75680ae7d03a146995196ab958acb006f
mohsen <mamaddnet@gmail.com>
2023-09-01 04:31:55 +0330

    Get data from database

--- index.js
+++ index.js
@@ -26,3 +26,37 @@ class Response{
         return JSON.stringify(this);
     }
 }
+
+// Get data from mariadb database
+async function getData(numidFrom = 0, number=6, classification = undefined){
+    
+    const classificationCondition = classification ? 'AND classification=' + `"${classification}"`: '';
+
+    let query = `\
+    SELECT DISTINCT numid, classification, title, iiifurl FROM data \
+    WHERE (numid >= ${numidFrom} ${classificationCondition})\
+    LIMIT ${number};`;
+
+    const connection = await mariadb.createConnection({
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: 'imageInfo',
+        // timeout : process.env.DATABASE_TIMEOUT_LIMIT,
+        // connectionLimit:process.env.DATABASE_CONNECTION_LIMIT,
+        // trace: true,
+    })
+
+    try{
+        
+        
+        const result = await connection.query(query);
+        connection.end();
+        return result;
+
+    }catch(err){
+        throw err;
+    }
+
+}
