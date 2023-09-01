938fcd78ceed847b1343b562b2fa9d2d0f51e14a
mohsen <mamaddnet@gmail.com>
2023-09-01 23:50:06 +0330

    Append image to html

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 
+const imageContainer = document.querySelector(".grid-wrapper")
 
 async function getData(lastNumid=0, number=10, classification =''){
     const APIurl = new URL("https://paint.iran.liara.run")
@@ -10,5 +11,17 @@ async function getData(lastNumid=0, number=10, classification =''){
     const response = await fetch(APIurl);
     const data = await response.json();
     return data;
+}
+
+
+async function imgAppend(data){
+    data.forEach(info => {
+        const img = document.createElement("img");
+        img.src = `${info.iiifurl}/full/1000,/0/default.jpg`
+        const div = document.createElement("div");
+        div.classList.add('tall')
+        div.appendChild(img);
+        imageContainer.appendChild(div);
+    })
 }
 
