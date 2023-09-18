630d4f2923ae9dc4b067f0cbcec1db32bdcf476a
mohsen <mamaddnet@gmail.com>
2023-09-18 19:52:46 +0330

    Specifying the classification of images displayed when page load

--- public/script.js
+++ public/script.js
@@ -3,7 +3,9 @@ const imgIncrease = 80;
 let lastNumid = 0;
 const THUMB_WIDTH = 600; // Max thumb width
 const THUMB_HEIGHT = 600; // Max thumb height
-let classification = 'Painting';
+
+const classificationSwitch = document.querySelector('.tgl');
+let classification = classificationSwitch.checked ? 'Drawing' : 'Painting';
 
 async function getData(lastNumid = 0, number = 10, classification = "") {
     const APIurl = new URL("https://paint.iran.liara.run");
@@ -159,7 +161,6 @@ function setImageClass(classification){
     })
 }
 
-const classificationSwitch = document.querySelector('.tgl');
 
 classificationSwitch.addEventListener('change', ()=>{
     window.scrollTo(0, 0)
